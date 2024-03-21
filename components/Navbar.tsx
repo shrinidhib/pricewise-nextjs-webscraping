97c3521d4be0405b631f875858790f3ee174e00a
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-21 11:08:04 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const navIcons
+const navIcons=[
+    { src: 'assets/icons/search.svg', alt: 'search'},
+    { src: 'assets/icons/black-heart.svg', alt: 'heart'},
+    { src: 'assets/icons/user.svg', alt: 'user'},
+]
 
 
 const Navbar = () => {
@@ -17,10 +21,13 @@ const Navbar = () => {
                 </p>
             </Link>
             <div className='flex items-center gap-5'>
-                <ul>
-
-                </ul>
-
+               {navIcons.map((icon)=>(
+                <Image key={icon.alt}
+                src={icon.src}
+                alt={icon.alt}
+                width={28} height={28}
+                className='object-contain'/>
+               ))}
             </div>
         </nav>
     </header>

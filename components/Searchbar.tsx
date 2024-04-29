7f18d92516d6134eccaf2ec0ca95af15b7800800
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-04-29 10:27:54 +0530

    search bar frontend functionality

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,40 @@
 "use client"
 
+import { FormEvent, useState } from "react"
+
+const isValidAmazonProductLink=(url: string)=>{
+    try{
+      const parsedURL = new URL(url)
+      const hostname=parsedURL.hostname
+      if (hostname.includes('amazon.com') || 
+      hostname.includes('amazon.') || 
+      hostname.endsWith('amazon')){
+        return true;
+      }
+    }catch(error){
+      return false;
+    }
+    return false;
+}
+
 const Searchbar = () => {
-    const handleSubmit=()=>{
+  const [searchPrompt,setSearchPrompt]=useState('')
+  const [isLoading,setIsLoading]=useState(false)
+
+  const handleSubmit=(event: FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+
+    const isValidLink=isValidAmazonProductLink(searchPrompt)
+    if (!isValidLink){
+      return alert('Please provide a valid Amazon link')
+    }
+    try{
+        setIsLoading(true)
+    }catch(error){
+        console.log(error)
+    }finally{
+      setIsLoading(false)
+    }
 
     }
   return (
@@ -10,9 +43,13 @@ const Searchbar = () => {
         <input
         type="text"
         placeholder="Enter product link"
-        className="searchbar-input"></input>
+        className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e)=>{setSearchPrompt(e.target.value)}}
+        />
         <button type="submit"
-        className="searchbar-btn">Search</button>
+        disabled={searchPrompt===''}
+        className="searchbar-btn">{isLoading? 'Searching...': 'Search'}</button>
     </form>
   )
 }
